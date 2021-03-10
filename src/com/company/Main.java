d303041671da298244b75fb18f8078407165e151
JakubTesar <jakub.tesar@educanet.cz>
2021-03-10 16:12:49 +0100

    První část

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        LinearFunction functionVariable = new LinearFunction();
+
+        ArrayList<LinearFunction> arrFunctions = new ArrayList<LinearFunction>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("functions.txt"));
+            String nextLine = br.readLine();
+
+            while(nextLine != null) {
+                nextLine = br.readLine();
+                functionVariable.functionClass = nextLine;
+                arrFunctions.add(functionVariable);
+            }
+        } catch (IOException e) {}
+
+        for (int i = 0; i < arrFunctions.size(); i++) {
+            System.out.println(arrFunctions.get(i));
+        }
 
     }
 }
